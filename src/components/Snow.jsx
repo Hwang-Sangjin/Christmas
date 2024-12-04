1a413ad19364c3dc887f7095589eda5cf2880d7e
Sangjin Hwang <aosdb5640@naver.com>
2024-12-04 15:29:34 +0900

    add: snow particle and music play bug fix

--- src/components/Snow.jsx
+++ src/components/Snow.jsx
@@ -1,5 +1,5 @@
 import { useFrame } from "@react-three/fiber";
-import { useMemo, useRef } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import * as THREE from "three";
 import particlesVertexShader from "../shaders/particles/vertex.glsl";
 import particlesFragmentShader from "../shaders/particles/fragment.glsl";
@@ -22,55 +22,58 @@ const Snow = () => {
     }),
     []
   );
+  const [SnowGeometry, setSnowGeometry] = useState(new THREE.BufferGeometry());
+  const particle_cnt = 2000;
+
+  useEffect(() => {
+    const temp = new THREE.BufferGeometry();
+    const positionArray = new Float32Array(particle_cnt * 3);
+    const progressArray = new Float32Array(particle_cnt);
+    const sizeArray = new Float32Array(particle_cnt);
+    const alphaArray = new Float32Array(particle_cnt);
+    const IndexArray = new Float32Array(particle_cnt);
+
+    for (let i = 0; i < particle_cnt; i++) {
+      positionArray[i * 3] = (Math.random() - 0.5) * 100;
+      positionArray[i * 3 + 1] = Math.random() * 30;
+      positionArray[i * 3 + 2] = -Math.random() * 50;
+      progressArray[i] = Math.random();
+
+      sizeArray[i] = Math.random();
+
+      alphaArray[i] = Math.random();
+
+      IndexArray[i] = Math.random();
+    }
+
+    temp.setAttribute(
+      "position",
+      new THREE.BufferAttribute(new Float32Array(positionArray), 3)
+    );
+    temp.setAttribute(
+      "aProgress",
+      new THREE.BufferAttribute(new Float32Array(progressArray), 1)
+    );
+    temp.setAttribute(
+      "aSize",
+      new THREE.BufferAttribute(new Float32Array(sizeArray), 1)
+    );
+    temp.setAttribute(
+      "aAlpha",
+      new THREE.BufferAttribute(new Float32Array(alphaArray), 1)
+    );
+    temp.setAttribute(
+      "aIndex",
+      new THREE.BufferAttribute(new Float32Array(IndexArray), 1)
+    );
+
+    setSnowGeometry(temp);
+  }, []);
 
   useFrame((state) => {
     const { clock } = state;
     particleUniforms.uTime.value = clock.getElapsedTime();
   });
-
-  const SnowGeometry = new THREE.BufferGeometry();
-
-  const particle_cnt = 2000;
-
-  const positionArray = new Float32Array(particle_cnt * 3);
-  const progressArray = new Float32Array(particle_cnt);
-  const sizeArray = new Float32Array(particle_cnt);
-  const alphaArray = new Float32Array(particle_cnt);
-  const IndexArray = new Float32Array(particle_cnt);
-
-  for (let i = 0; i < particle_cnt; i++) {
-    positionArray[i * 3] = (Math.random() - 0.5) * 100;
-    positionArray[i * 3 + 1] = Math.random() * 30;
-    positionArray[i * 3 + 2] = -Math.random() * 50;
-    progressArray[i] = Math.random();
-
-    sizeArray[i] = Math.random();
-
-    alphaArray[i] = Math.random();
-
-    IndexArray[i] = Math.random();
-  }
-
-  SnowGeometry.setAttribute(
-    "position",
-    new THREE.BufferAttribute(new Float32Array(positionArray), 3)
-  );
-  SnowGeometry.setAttribute(
-    "aProgress",
-    new THREE.BufferAttribute(new Float32Array(progressArray), 1)
-  );
-  SnowGeometry.setAttribute(
-    "aSize",
-    new THREE.BufferAttribute(new Float32Array(sizeArray), 1)
-  );
-  SnowGeometry.setAttribute(
-    "aAlpha",
-    new THREE.BufferAttribute(new Float32Array(alphaArray), 1)
-  );
-  SnowGeometry.setAttribute(
-    "aIndex",
-    new THREE.BufferAttribute(new Float32Array(IndexArray), 1)
-  );
 
   return (
     <points ref={meshRef} geometry={SnowGeometry}>

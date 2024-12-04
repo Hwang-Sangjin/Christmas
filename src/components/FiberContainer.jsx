441f62050e8894ce2fc579952336414ee4a64df4
Sangjin Hwang <aosdb5640@naver.com>
2024-12-04 17:29:17 +0900

    add: fog modify

--- src/components/FiberContainer.jsx
+++ src/components/FiberContainer.jsx
@@ -14,14 +14,15 @@ export default function FiberContainer() {
   const cameraControlRef = useRef(null);
 
   return (
-    <Canvas camera={{ position: [0, 10, 0], fov: 45 }} shadows>
+    <Canvas camera={{ position: [-40, 10, -10], fov: 45 }} shadows>
+      <fog attach="fog" args={["#697d7e", 20, 80]} />
       <Frame />
       <Ground />
       <Snow />
       <Environment preset="sunset" backgroundBlurriness={0.1} />
       <directionalLight position={[6.25, 3, 4]} color="white" intensity={1.2} />
       <OrbitControls
-        target={[0, 0, -50]}
+        target={[-10, 2, -50]}
         ref={cameraControlRef}
         enablePan={true}
         minDistance={1}

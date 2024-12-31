4b6c1a4f925d321fd1175892a96b7359ca3c96de
Sangjin Hwang <aosdb5640@naver.com>
2024-12-31 14:49:47 +0900

    add: loading page

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,13 +1,25 @@
 import * as React from "react";
 import { useProgress } from "@react-three/drei";
 import "./Loader.css";
+import { useEffect } from "react";
+import { useRef } from "react";
+import { gsap } from "gsap";
 
 export function Loader() {
+  const loadingRef = useRef();
   const { active, progress } = useProgress();
 
-  return active ? (
-    <div className="loader">
-      <span>{Math.floor(progress)}%</span>
-    </div>
-  ) : null;
+  useEffect(() => {
+    if (progress === 100) {
+      if (loadingRef.current !== null) {
+        console.log(progress, loadingRef);
+        loadingRef.current.classList.add("ended");
+        loadingRef.current.style.transform = "";
+      }
+    } else if (loadingRef) {
+      loadingRef.current.style.transform = `scaleX(${progress / 100})`;
+    }
+  }, [progress]);
+
+  return active ? <div ref={loadingRef} className="loading-bar"></div> : null;
 }
